dcd8a11063581a222874b5ae10a407cc4789d190
Katrina Cooper <kcooper11@student.cvtc.edu>
2022-11-12 17:53:23 -0600

    add server and index.ejs

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5163;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .use(express.json())
+  .use(express.urlencoded({extended: true}))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", async(req, res) => {
+    const args = {
+      time: Date.now()
+    }
+    res.render("pages/index", args);
+  })
+  .listen(PORT, () => console.log(`listening on ${PORT}`));
