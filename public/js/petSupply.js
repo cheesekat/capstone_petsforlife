01dbc02fa6e3511d4b7e88ba9463c86c8b921758
Nicholas Collier <colliernw@gmail.com>
2022-12-05 22:00:22 -0600

    Updated supplies

--- public/js/petSupply.js
+++ public/js/petSupply.js
@@ -1,17 +1,12 @@
-const supply = document.getElementById("supplyForm");
 
-supply.addEventListener("find", supplyLinks);
-
+function supplyLinks() {
 
 const animal = document.querySelector("#animalType").value;
 const output = document.querySelector("#output");
 
-
-
-function supplyLinks(animal) {
 switch (animal) {
-    case dog: 
-    output.innerHTML += (`<ul id="dogLinks">
+    case ("dog"): //Dog
+    output.innerHTML = (`<ul id="dogLinks">
     <li><a href="https://www.purina.com/dogs">Purina</a></li><br></br>
     <li><a href="https://www.chewy.com/b/dog-288">Chewy</a></li><br></br>
     <li><a href="https://www.petflow.com/dog">PetFlow</a></li><br></br>
@@ -21,8 +16,8 @@ switch (animal) {
     </ul>`);
   
     break;
-    case animal.cat:
-    results.innerHTML =  (`<ul id="catLinks">
+    case ("cat"):  //Cat
+    output.innerHTML =  (`<ul id="catLinks">
       <li><a href="https://www.purina.com/cats">Purina</a></li><br></br>
       <li><a href="https://www.chewy.com/b/cat-325">Chewy</a></li><br></br>
       <li><a href="https://www.petflow.com/cat">PetFlow</a></li><br></br>
@@ -32,8 +27,8 @@ switch (animal) {
       </ul>`);
       
       break;
-    case animal.bird:
-        results.innerHTML = ` <ul id="birdLinks">
+    case ("bird"):  // bird
+        output.innerHTML = ` <ul id="birdLinks">
         <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-bird">Tractor Supply</a></li><br></br>
         <li><a href="https://www.chewy.com/b/bird-941">Chewy</a></li><br></br>
         <li><a href="https://www.petflow.com/bird">PetFlow</a></li><br></br>
@@ -43,8 +38,8 @@ switch (animal) {
         </ul>`;
         
         break;
-    case animal.farm:
-            results.innerHTML =  `  <ul id="farmLinks">
+    case ("farm"):  //farm
+            output.innerHTML =  `  <ul id="farmLinks">
           <li><a href="https://www.tractorsupply.com/tsc/catalog/poultry-livestock">Tractor Supply</a></li><br></br>
           <li><a href="https://www.chewy.com/b/farm-animal-8403">Chewy</a></li><br></br>
           <li><a href="https://www.farmtek.com/farm/supplies/cat1;;ft_livestock_supplies.html">Farm Tek</a></li><br></br>
@@ -52,8 +47,8 @@ switch (animal) {
           <li><a href="https://www.petco.com/shop/en/petcostore/category/farm-and-feed">Petco</a></li><br></br>
           </ul>`;
           break;
-    case animal.horse:
-            results.innerHTML = `<ul id="horseLinks">
+    case ("horse"):  //Horse
+            output.innerHTML = `<ul id="horseLinks">
             <li><a href="https://www.tractorsupply.com/tsc/cms/horse-essentials">Tractor Supply</a></li><br></br>
             <li><a href="https://www.chewy.com/b/horse-1663">Chewy</a></li><br></br>
             <li><a href="https://www.entirelypets.com/horseproducts.html">Entirerly Pets</a></li><br></br>
@@ -62,8 +57,8 @@ switch (animal) {
             </ul>`;
             
             break;
-            case animal.fish:
-                results.innerHTML = `<ul id="fishLinks">
+            case ("fish"):  //Fish
+                output.innerHTML = `<ul id="fishLinks">
               <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-fish">Tractor Supply</a></li><br></br>
               <li><a href="https://www.chewy.com/b/fish-885">Chewy</a></li><br></br>
               <li><a href="https://www.petflow.com/fish">PetFlow</a></li><br></br>
@@ -73,8 +68,8 @@ switch (animal) {
               </ul>`;
               
               break;
-              case animal.reptile:
-                results.innerHTML = ` <ul id="reptileLinks">
+              case ("reptile"):  //Reptile
+                output.innerHTML = ` <ul id="reptileLinks">
                 <li><a href="https://www.tractorsupply.com/tsc/catalog/reptile">Tractor Supply</a></li><br></br>
                 <li><a href="https://www.chewy.com/b/reptile-1025">Chewy</a></li><br></br>
                 <li><a href="https://www.petflow.com/reptile">PetFlow</a></li><br></br>
@@ -84,8 +79,8 @@ switch (animal) {
                 </ul>`;
                 
                 break;
-                case animal.smallPet:
-                    results.innerHTML = `<ul id="smallPetLinks">
+                case ("smallPet"):  //Small Pet
+                    output.innerHTML = `<ul id="smallPetLinks">
                   <li><a href="https://www.tractorsupply.com/tsc/catalog/small-pet">Tractor Supply</a></li><br></br>
                   <li><a href="https://www.chewy.com/b/small-pet-977">Chewy</a></li><br></br>
                   <li><a href="https://www.petflow.com/small-pet">PetFlow</a></li><br></br>
@@ -96,14 +91,14 @@ switch (animal) {
                   
                   break;
                   default:
-                    // results.innerHTML =  `<ul id="links">
-                    //     <li><a href="https://www.purina.com/">Purina</a></li><br></br>
-                    //     <li><a href="https://www.chewy.com/">Chewy</a></li><br></br>
-                    //     <li><a href="https://www.petflow.com/">PetFlow</a></li><br></br>
-                    //     <li><a href="https://www.petsuppliesplus.com/online-ordering">Pet Supplies Plus</a></li><br></br>
-                    //     <li><a href="https://www.petco.com/shop/en/petcostore">Petco</a></li><br></br>
-                    //     <li><a href="https://www.entirelypets.com/suppliesplus.html">EntirelyPets</a></li>
-                    // </ul>`
+                    output.innerHTML =  `<ul id="links">
+                        <li><a href="https://www.purina.com/">Purina</a></li><br></br>
+                        <li><a href="https://www.chewy.com/">Chewy</a></li><br></br>
+                        <li><a href="https://www.petflow.com/">PetFlow</a></li><br></br>
+                        <li><a href="https://www.petsuppliesplus.com/online-ordering">Pet Supplies Plus</a></li><br></br>
+                        <li><a href="https://www.petco.com/shop/en/petcostore">Petco</a></li><br></br>
+                        <li><a href="https://www.entirelypets.com/suppliesplus.html">EntirelyPets</a></li>
+                    </ul>`
                     ;
     }
   }
