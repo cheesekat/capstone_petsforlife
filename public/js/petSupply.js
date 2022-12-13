17c060bca7f827fe15b418c5608e40e687b96f7d
Katrina Cooper <kcooper11@student.cvtc.edu>
2022-12-12 18:18:55 -0600

    [Finishes #183868338] open links in new tab

--- public/js/petSupply.js
+++ public/js/petSupply.js
@@ -7,12 +7,12 @@ function supplyLinks() {
   switch (animal) {
     case ("dog"): //Dog
       output.innerHTML = (`<ul id="dogLinks">
-      <li><a href="https://www.purina.com/dogs">Purina</a></li><br>
-      <li><a href="https://www.chewy.com/b/dog-288">Chewy</a></li><br>
-      <li><a href="https://www.petflow.com/dog">PetFlow</a></li><br>
-      <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-      <li><a href="https://www.petco.com/shop/en/petcostore/category/dog">Petco</a></li><br>
-      <li><a href="https://www.entirelypets.com/dogsupplies.html">EntirelyPets</a></li>
+      <li><a href="https://www.purina.com/dogs" target="_blank">Purina</a></li><br>
+      <li><a href="https://www.chewy.com/b/dog-288" target="_blank">Chewy</a></li><br>
+      <li><a href="https://www.petflow.com/dog" target="_blank">PetFlow</a></li><br>
+      <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+      <li><a href="https://www.petco.com/shop/en/petcostore/category/dog" target="_blank">Petco</a></li><br>
+      <li><a href="https://www.entirelypets.com/dogsupplies.html" target="_blank">EntirelyPets</a></li><br>
       <li>
         <a href="images/dogs.png" download="dogchecklist">
           <img src="images/dogs.png" alt="dogchecklist" width=250px" height="350px">
@@ -23,12 +23,12 @@ function supplyLinks() {
     break;
     case ("cat"):  //Cat
     output.innerHTML =  (`<ul id="catLinks">
-      <li><a href="https://www.purina.com/cats">Purina</a></li><br>
-      <li><a href="https://www.chewy.com/b/cat-325">Chewy</a></li><br>
-      <li><a href="https://www.petflow.com/cat">PetFlow</a></li><br>
-      <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-      <li><a href="https://www.petco.com/shop/en/petcostore/category/cat">Petco</a></li><br>
-      <li><a href="https://www.entirelypets.com/catsupplies.html">EntirelyPets</a></li>
+      <li><a href="https://www.purina.com/cats" target="_blank">Purina</a></li><br>
+      <li><a href="https://www.chewy.com/b/cat-325" target="_blank">Chewy</a></li><br>
+      <li><a href="https://www.petflow.com/cat" target="_blank">PetFlow</a></li><br>
+      <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+      <li><a href="https://www.petco.com/shop/en/petcostore/category/cat" target="_blank">Petco</a></li><br>
+      <li><a href="https://www.entirelypets.com/catsupplies.html" target="_blank">EntirelyPets</a></li><br>
       <li>
           <a href="images/cats.png" download="catchecklist">
             <img src="images/cats.png" alt="catschecklist" width=250px" height="350px">
@@ -39,12 +39,12 @@ function supplyLinks() {
       break;
     case ("bird"):  // bird
         output.innerHTML = ` <ul id="birdLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-bird">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/bird-941">Chewy</a></li><br>
-        <li><a href="https://www.petflow.com/bird">PetFlow</a></li><br>
-        <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/bird">Petco</a></li><br>
-        <li><a href="https://www.entirelypets.com/birdproducts.html">EntirelyPets</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-bird" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/bird-941" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.petflow.com/bird" target="_blank">PetFlow</a></li><br>
+        <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/bird" target="_blank">Petco</a></li><br>
+        <li><a href="https://www.entirelypets.com/birdproducts.html" target="_blank">EntirelyPets</a></li><br>
         <li>
           <a href="images/birds.png" download="birdchecklist">
             <img src="images/birds.png" alt="birdchecklist" width=250px" height="350px">
@@ -55,11 +55,11 @@ function supplyLinks() {
         break;
     case ("farm"):  //farm
       output.innerHTML =  `  <ul id="farmLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/catalog/poultry-livestock">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/farm-animal-8403">Chewy</a></li><br>
-        <li><a href="https://www.farmtek.com/farm/supplies/cat1;;ft_livestock_supplies.html">Farm Tek</a></li><br>
-        <li><a href="https://www.farmandfleet.com/farm-and-livestock/">Farm and Fleet</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/farm-and-feed">Petco</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/catalog/poultry-livestock" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/farm-animal-8403" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.farmtek.com/farm/supplies/cat1;;ft_livestock_supplies.html" target="_blank">Farm Tek</a></li><br>
+        <li><a href="https://www.farmandfleet.com/farm-and-livestock/" target="_blank">Farm and Fleet</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/farm-and-feed" target="_blank">Petco</a></li><br>
         <li>
           <a href="images/barnyards.png" download="barnyardchecklist">
             <img src="images/barnyards.png" alt="barnyardchecklist" width=250px" height="350px">
@@ -69,11 +69,11 @@ function supplyLinks() {
       break;
     case ("horse"):  //Horse
       output.innerHTML = `<ul id="horseLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/cms/horse-essentials">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/horse-1663">Chewy</a></li><br>
-        <li><a href="https://www.entirelypets.com/horseproducts.html">Entirerly Pets</a></li><br>
-        <li><a href="https://www.farmandfleet.com/horse-care-and-tack/">Farm and Fleet</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/farm-and-feed/horse-shop">Petco</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/cms/horse-essentials" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/horse-1663" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.entirelypets.com/horseproducts.html" target="_blank">Entirerly Pets</a></li><br>
+        <li><a href="https://www.farmandfleet.com/horse-care-and-tack/" target="_blank">Farm and Fleet</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/farm-and-feed/horse-shop" target="_blank">Petco</a></li><br>
         <li>
           <a href="images/horses.png" download="horsechecklist">
             <img src="images/horses.png" alt="horsechecklist" width=250px" height="350px">
@@ -84,23 +84,23 @@ function supplyLinks() {
       break;
     case ("fish"):  //Fish
       output.innerHTML = `<ul id="fishLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-fish">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/fish-885">Chewy</a></li><br>
-        <li><a href="https://www.petflow.com/fish">PetFlow</a></li><br>
-        <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/fish">Petco</a></li><br>
-        <li><a href="https://www.entirelypets.com/aquarium-supplies.html">EntirelyPets</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/catalog/pet-fish" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/fish-885" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.petflow.com/fish" target="_blank">PetFlow</a></li><br>
+        <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/fish" target="_blank">Petco</a></li><br>
+        <li><a href="https://www.entirelypets.com/aquarium-supplies.html" target="_blank">EntirelyPets</a></li><br>
       </ul>`;
               
       break;
     case ("reptile"):  //Reptile
       output.innerHTML = ` <ul id="reptileLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/catalog/reptile">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/reptile-1025">Chewy</a></li><br>
-        <li><a href="https://www.petflow.com/reptile">PetFlow</a></li><br>
-        <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/reptile">Petco</a></li><br>
-        <li><a href="https://www.entirelypets.com/repprod.html">EntirelyPets</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/catalog/reptile" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/reptile-1025" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.petflow.com/reptile" target="_blank">PetFlow</a></li><br>
+        <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/reptile" target="_blank">Petco</a></li><br>
+        <li><a href="https://www.entirelypets.com/repprod.html" target="_blank">EntirelyPets</a></li><br>
         <li>
           <a href="images/scalesfins1.png" download="scalesfinschecklist1">
             <img src="images/scalesfins1.png" alt="scalesfinschecklist1" width=250px" height="350px">
@@ -117,12 +117,12 @@ function supplyLinks() {
       break;
     case ("smallPet"):  //Small Pet
       output.innerHTML = `<ul id="smallPetLinks">
-        <li><a href="https://www.tractorsupply.com/tsc/catalog/small-pet">Tractor Supply</a></li><br>
-        <li><a href="https://www.chewy.com/b/small-pet-977">Chewy</a></li><br>
-        <li><a href="https://www.petflow.com/small-pet">PetFlow</a></li><br>
-        <li><a href="https://www.petsuppliesplus.com/">Pet Supplies Plus</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore/category/small-animal">Petco</a></li><br>
-        <li><a href="https://www.entirelypets.com/birandotpet.html">EntirelyPets</a></li>
+        <li><a href="https://www.tractorsupply.com/tsc/catalog/small-pet" target="_blank">Tractor Supply</a></li><br>
+        <li><a href="https://www.chewy.com/b/small-pet-977" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.petflow.com/small-pet" target="_blank">PetFlow</a></li><br>
+        <li><a href="https://www.petsuppliesplus.com/" target="_blank">Pet Supplies Plus</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore/category/small-animal" target="_blank">Petco</a></li><br>
+        <li><a href="https://www.entirelypets.com/birandotpet.html" target="_blank">EntirelyPets</a></li><br>
         <li>
           <a href="images/smallfurry1.png" download="smallfurrychecklist1">
             <img src="images/smallfurry1.png" alt="smallfurrychecklist1" width=250px" height="350px">
@@ -148,12 +148,12 @@ function supplyLinks() {
       break;
     default:
       output.innerHTML =  `<ul id="links">
-        <li><a href="https://www.purina.com/">Purina</a></li><br>
-        <li><a href="https://www.chewy.com/">Chewy</a></li><br>
-        <li><a href="https://www.petflow.com/">PetFlow</a></li><br>
-        <li><a href="https://www.petsuppliesplus.com/online-ordering">Pet Supplies Plus</a></li><br>
-        <li><a href="https://www.petco.com/shop/en/petcostore">Petco</a></li><br>
-        <li><a href="https://www.entirelypets.com/suppliesplus.html">EntirelyPets</a></li>
+        <li><a href="https://www.purina.com/" target="_blank">Purina</a></li><br>
+        <li><a href="https://www.chewy.com/" target="_blank">Chewy</a></li><br>
+        <li><a href="https://www.petflow.com/" target="_blank">PetFlow</a></li><br>
+        <li><a href="https://www.petsuppliesplus.com/online-ordering" target="_blank">Pet Supplies Plus</a></li><br>
+        <li><a href="https://www.petco.com/shop/en/petcostore" target="_blank">Petco</a></li><br>
+        <li><a href="https://www.entirelypets.com/suppliesplus.html" target="_blank">EntirelyPets</a></li>
       </ul>`;
     }
   }
